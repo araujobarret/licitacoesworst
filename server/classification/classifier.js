5bf56ab30affcf99a3fb8da5fdcd6d6b95584076
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-09-22 17:14:09 -0300

    Classifier improvements

--- server/classification/classifier.js
+++ server/classification/classifier.js
@@ -10,94 +10,107 @@ let probabilityModel = [
 let bayesianModel = {
   classe: probabilityModel,
   memoria: [
-    { nome: '(-inf-0.5]', probability: probabilityModel},
-    { nome: '(0.5-1.5]',  probability: probabilityModel},
-    { nome: '(1.5-5]', probability: probabilityModel},
-    { nome: '(5-12]', probability: probabilityModel},
-    { nome: '(12-inf)', probability: probabilityModel}
+    {
+      nome: '(-inf-0.5]',
+      probability: [
+        {nome: 'economico', probability: 0.0625},
+        {nome: 'comum', probability: 0.017241379},
+        {nome: 'alto', probability: 0.025641026},
+        {nome: 'altissimo', probability: 0.148148148}
+      ]
+    },
+    { nome: '(0.5-1.5]',
+      probability: [
+        {nome: 'economico', probability: 0.75},
+        {nome: 'comum', probability: 0.017241379},
+        {nome: 'alto', probability: 0.025641026},
+        {nome: 'altissimo', probability: 0.037037037}
+      ]
+    },
+    { nome: '(1.5-5]',
+      probability: [
+        {nome: 'economico', probability: 0.0625},
+        {nome: 'comum', probability: 0.8448275860},
+        {nome: 'alto', probability: 0.076923077},
+        {nome: 'altissimo', probability: 0.037037037}
+      ]
+    },
+    { nome: '(5-12]',
+      probability: [
+        {nome: 'economico', probability: 0.0625},
+        {nome: 'comum', probability: 0.0625},
+        {nome: 'alto', probability: 0.794871795},
+        {nome: 'altissimo', probability: 0.111111111}
+      ]
+    },
+    { nome: '(12-inf)',
+      probability: [
+        {nome: 'economico', probability: 0.0625},
+        {nome: 'comum', probability: 0.017241379},
+        {nome: 'alto', probability: 0.0625},
+        {nome: 'altissimo', probability: 0.666666667}
+      ]
+    }
   ],
   processador: [
-    { nome: 'comum', probability: probabilityModel},
-    { nome: 'alto', probability: probabilityModel},
-    { nome: 'altissimo', probability: probabilityModel}
+    { nome: 'comum',
+      probability: [
+        {nome: 'economico', probability: 0.357142857},
+        {nome: 'comum', probability: 0.571428571},
+        {nome: 'alto', probability: 0.027027027},
+        {nome: 'altissimo', probability: 0.04}
+      ]
+    },
+    { nome: 'alto',
+      probability: [
+        {nome: 'economico', probability: 0.571428571},
+        {nome: 'comum', probability: 0.410714286},
+        {nome: 'alto', probability: 0.702702703},
+        {nome: 'altissimo', probability: 0.28}
+      ]
+    },
+    { nome: 'altissimo',
+      probability: [
+        {nome: 'economico', probability: 0.071428571},
+        {nome: 'comum', probability: 0.017857143},
+        {nome: 'alto', probability: 0.27027027},
+        {nome: 'altissimo', probability: 0.68}
+      ]
+    }
   ],
   video: [
-    { nome: 'n', probability: probabilityModel},
-    { nome: 's', probability: probabilityModel}
+    { nome: 'n',
+      probability: [
+        {nome: 'economico', probability: 0.923076923},
+        {nome: 'comum', probability: 0.945454545},
+        {nome: 'alto', probability: 0.972222222},
+        {nome: 'altissimo', probability: 0.25}
+      ]
+    },
+    { nome: 's',
+      probability: [
+        {nome: 'economico', probability: 0.076923077},
+        {nome: 'comum', probability: 0.054545455},
+        {nome: 'alto', probability: 0.027777778},
+        {nome: 'altissimo', probability: 0.75}
+      ]
+    }
   ]
 };
 
-// Economic computer probabilities
-bayesianModel.memoria[0].probability[0].probability = 0.0625;
-bayesianModel.memoria[1].probability[0].probability = 0.75;
-bayesianModel.memoria[2].probability[0].probability = 0.0625;
-bayesianModel.memoria[3].probability[0].probability = 0.0625;
-bayesianModel.memoria[4].probability[0].probability = 0.0625;
-
-bayesianModel.processador[0].probability[0].probability = 0.357142857;
-bayesianModel.processador[1].probability[0].probability = 0.571428571;
-bayesianModel.processador[2].probability[0].probability = 0.071428571;
-
-bayesianModel.video[0].probability[0].probability = 0.923076923;
-bayesianModel.video[1].probability[0].probability = 0.076923077;
-
-// Common computer probabilities
-bayesianModel.memoria[0].probability[1].probability = 0.017241379;
-bayesianModel.memoria[1].probability[1].probability = 0.017241379;
-bayesianModel.memoria[2].probability[1].probability = 0.8448275860;
-bayesianModel.memoria[3].probability[1].probability = 0.103448277;
-bayesianModel.memoria[4].probability[1].probability = 0.017241379;
-
-bayesianModel.processador[0].probability[1].probability = 0.571428571;
-bayesianModel.processador[1].probability[1].probability = 0.410714286;
-bayesianModel.processador[2].probability[1].probability = 0.017857143;
-
-bayesianModel.video[0].probability[1].probability = 0.945454545;
-bayesianModel.video[1].probability[1].probability = 0.054545455;
-
-// High computer probabilities
-bayesianModel.memoria[0].probability[2].probability = 0.025641026;
-bayesianModel.memoria[1].probability[2].probability = 0.025641026;
-bayesianModel.memoria[2].probability[2].probability = 0.076923077;
-bayesianModel.memoria[3].probability[2].probability = 0.794871795;
-bayesianModel.memoria[4].probability[2].probability = 0.076923077;
-
-bayesianModel.processador[0].probability[2].probability = 0.027027027;
-bayesianModel.processador[1].probability[2].probability = 0.702702703;
-bayesianModel.processador[2].probability[2].probability = 0.27027027;
-
-bayesianModel.video[0].probability[2].probability = 0.972222222;
-bayesianModel.video[1].probability[2].probability = 0.027777778;
-
-// Very high computer probabilities
-bayesianModel.memoria[0].probability[3].probability = 0.148148148;
-bayesianModel.memoria[1].probability[3].probability = 0.037037037;
-bayesianModel.memoria[2].probability[3].probability = 0.037037037;
-bayesianModel.memoria[3].probability[3].probability = 0.111111111;
-bayesianModel.memoria[4].probability[3].probability = 0.666666667;
-
-bayesianModel.processador[0].probability[3].probability = 0.04;
-bayesianModel.processador[1].probability[3].probability = 0.28;
-bayesianModel.processador[2].probability[3].probability = 0.68;
-
-bayesianModel.video[0].probability[3].probability = 0.25;
-bayesianModel.video[1].probability[3].probability = 0.75;
-
 // (-inf-0.5]\\\'\',\'\\\'(0.5-1.5]\\\'\',\'\\\'(1.5-5]\\\'\',\'\\\'(5-12]\\\'\',\'\\\'(12-inf)\\\'\'
 let Classify = (data) => {
-  let maior = 0, index, processador, memoria, video, i;
+  let maior = 0, index, processador, memoria, video, i, soma = 0;
   let produtorio = [];
 
   let c = {
-    memoria: '(1.5-5]',
-    processador: 'comum',
-    video: 'n'
+    m: '(1.5-5]',
+    p: 'comum',
+    v: 'n'
   };
 
-  console.log(bayesianModel.memoria[0].probability[3]);
   for(i = 0; i < 4; i++){
-
-    switch(c.memoria){
+    switch(c.m){
       case '(-inf-0.5]':
         memoria = bayesianModel.memoria[0].probability[i].probability;
         break;
@@ -114,8 +127,7 @@ let Classify = (data) => {
         memoria = bayesianModel.memoria[4].probability[i].probability;
         break;
     }
-    console.log(bayesianModel.memoria[2].probability[i].probability);
-    switch(c.processador){
+    switch(c.p){
       case 'comum':
         processador = bayesianModel.processador[0].probability[i].probability;
         break;
@@ -126,21 +138,26 @@ let Classify = (data) => {
         processador = bayesianModel.processador[2].probability[i].probability;
         break;
     }
-    console.log(processador);
-    // switch(c.video){
-    //   case 'n':
-    //     video = bayesianModel.video[0].probability[i].probability;
-    //     break;
-    //   case 's':
-    //     video = bayesianModel.video[1].probability[i].probability;
-    //     break;
-    // }
+    switch(c.v){
+      case 'n':
+        video = bayesianModel.video[0].probability[i].probability;
+        break;
+      case 's':
+        video = bayesianModel.video[1].probability[i].probability;
+        break;
+    }
 
     //console.log('Model', JSON.stringify(bayesianModel.memoria, null, 2));
-    // console.log(memoria + ' x ' + processador + ' x ' + video + ' x ' + probabilityModel[i].probability);
+    console.log(memoria + ' x ' + processador + ' x ' + video + ' x ' + probabilityModel[i].probability);
     produtorio[i] = (memoria * processador * video * probabilityModel[i].probability).toFixed(10);
+    soma = parseFloat(produtorio[i]) + parseFloat(soma);
+    console.log('Produtorio', produtorio[i]);
   }
-  //console.log('Model', JSON.stringify(bayesianModel, null, 2));
+  console.log(soma);
+  console.log('1', produtorio[0]);
+  console.log('2', produtorio[1]/soma);
+  console.log('3', produtorio[2]/soma);
+  console.log('4', produtorio[3]/soma);
 };
 
 module.exports = {

c0d1a2f807ca0d25dac03297a216e26abfeea559
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-11-13 19:14:42 -0200

    Last changes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 const request = require('request');
+const hbs = require('hbs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
 const extractor = require('./classification/extractor');
 const normalizer = require('./classification/normalizer');
 const classifier = require('./classification/classifier');
@@ -11,7 +13,13 @@ const math = require('./classification/math');
 let app = express();
 const port = process.env.PORT || 3000;
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+app.use('/', express.static(path.join(__dirname + '/public')));
 app.use(bodyParser.json());
+
+hbs.registerHelper('logData', (data) => console.log(data));
 
 app.get('/get_items/:codigo_item_material', (req, res) => {
   let codigo_item_material = req.params.codigo_item_material;
@@ -31,8 +39,13 @@ app.get('/create_test/:codigo_item_material', (req, res) => {
     }).catch((e) => console.log(e));
 });
 
-app.get('/get_summary/', (req, res) => {
-  res.send(math.generateSummary());
+app.get('/get_summary', (req, res) => {
+  let summary = math.generateSummary();  
+  res.send(summary);
+  // res.render('summary.hbs', {
+  //   summary: 'Hi there',
+  //   data: summary
+  // });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
